2db9129e8205fa103b291bb4f006322c5c956ac0
smrut <smrutibhanja32@gmail.com>
2022-07-22 15:41:57 +0530

    Change video to static wallpaper

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-let taskbar = document.getElementsByClassName("taskbar")[0]
-let startmenu = document.getElementsByClassName("startmenu")[0]
+let taskbar = document.getElementsByClassName("taskbar")[0];
+let startmenu = document.getElementsByClassName("startmenu")[0];
 
-taskbar.addEventListener("click", ()=>{
-    if(startmenu.style.bottom == "50px"){
-        startmenu.style.bottom = "-590px"
-    }else{
-        startmenu.style.bottom = "50px"
-    }
-})
-
+taskbar.addEventListener("click", () => {
+  if (startmenu.style.bottom == "70px") {
+    startmenu.style.bottom = "-550px";
+  } else {
+    startmenu.style.bottom = "70px";
+  }
+});
